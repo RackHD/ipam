9591214f5d19868fe89cd9976279561881e79050
Joe Golio <joseph.golio@dell.com>
2016-09-21 08:49:26 -0700

    Add some comments to the index to help with debugging potential issues with DNS. Also enable mgoDebug inside the MongoDB Go driver.

--- main.go
+++ main.go
@@ -21,6 +21,17 @@ func init() {
 }
 
 func main() {
+
+	// Default to enable mgo debug. Set to false to disable.
+	var mgoDebug = true
+
+	if mgoDebug {
+        	mgo.SetDebug(true)
+        	var aLogger *log.Logger
+        	aLogger = log.New(os.Stderr, "", log.LstdFlags)
+        	mgo.SetLogger(aLogger)
+	}
+
 	// Start off with a new mux router.
 	router := mux.NewRouter().StrictSlash(true)
 
